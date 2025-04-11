372c1ac551e0d73c00789991661e80b6321677cc
i.kremenetskiy <i.kremenetskiy@tinkoff.ru>
2025-04-11 20:08:20 +0300

    сделал деку, пока не использую никак модельЭ

--- handlers/game.go
+++ handlers/game.go
@@ -1,1 +1,50 @@
 package handlers
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"fmt"
+	"hearthstone-clone-backend/models"
+
+	"github.com/google/uuid"
+)
+
+// NewDeck generates a new deck of cards
+func NewDeck() (*models.Deck, error) {
+	cards := []models.Card{
+		{ID: uuid.New().String(), Name: "Fireball", ManaCost: 4, Type: "spell"},
+		{ID: uuid.New().String(), Name: "Frostbolt", ManaCost: 2, Type: "spell"},
+		{ID: uuid.New().String(), Name: "Grizzly Bear", ManaCost: 3, Type: "minion"},
+		{ID: uuid.New().String(), Name: "Fire Elemental", ManaCost: 5, Type: "minion"},
+		// Add more cards as needed
+	}
+
+	// Shuffle the deck using crypto/rand
+	if err := shuffleDeck(cards); err != nil {
+		return nil, fmt.Errorf("failed to shuffle deck: %w", err)
+	}
+
+	return &models.Deck{Cards: cards}, nil
+}
+
+// shuffleDeck shuffles the slice of cards using crypto/rand
+func shuffleDeck(cards []models.Card) error {
+	for i := range cards {
+		j, err := randomInt(i + 1)
+		if err != nil {
+			return err
+		}
+		cards[i], cards[j] = cards[j], cards[i]
+	}
+	return nil
+}
+
+// randomInt generates a random integer between 0 and max using crypto/rand
+func randomInt(max int) (int, error) {
+	var num uint32
+	err := binary.Read(rand.Reader, binary.LittleEndian, &num)
+	if err != nil {
+		return 0, err
+	}
+	return int(num) % max, nil
+}
--- models/models.go
+++ models/models.go
@@ -5,8 +5,6 @@ type Card struct {
 	ID       string `json:"id"`
 	Name     string `json:"name"`
 	Type     string `json:"type"` // "minion" or "spell"
-	Attack   int    `json:"attack"`
-	Health   int    `json:"health"`
 	ManaCost int    `json:"mana_cost"`
 }
 
@@ -15,10 +13,16 @@ type Player struct {
 	Health int    `json:"health"`
 	Hand   []Card `json:"hand"`
 	Board  []Card `json:"board"`
+	Deck   Deck   `json:"deck"`
 }
 
 type Room struct {
 	ID         string   `json:"id"`
 	InviteLink string   `json:"invite_link"`
 	Players    []Player `json:"players"` // Track players in the room
+}
+
+// Deck represents a collection of cards
+type Deck struct {
+	Cards []Card
 }
